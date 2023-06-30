34dd4b50b6163ab746f1406913296d8fa89b904d
Apu Chandraw Shill <apu3660@gmail.com>
2023-06-30 13:08:29 +0600

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -2,7 +2,7 @@ from data import processor
 from data import config
 
 """
-This script will process the dataset and save them into two format. 1. CSV format, 2. TSV format
+This script will process the dataset and save them in two formats. 1. CSV format, 2. TSV format
 """
 
 file_names = [config.FILE_NER1, config.FILE_NER2]
@@ -10,11 +10,6 @@ file_names = [config.FILE_NER1, config.FILE_NER2]
 new_file_names = [config.NEW_FILE_NER1, config.NEW_FILE_NER2]
 
 up_file_names = [config.UP_FILE_NER1, config.UP_FILE_NER2]
-
-# print(file_names)
-# print(new_file_names)
-# print(up_file_names)
-
 
 for i, (fn, nfn, ufn) in enumerate(zip(file_names, new_file_names, up_file_names)):
     print(i, "\t", fn, "\t", nfn, "\t", ufn)
